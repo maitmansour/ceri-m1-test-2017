6bb6ff21a1c9d6bde0df23ccf8f992d7f911ecbf
AIT MANSOUR Mohamed <mohamedaitmansour95@gmail.com>
2018-04-09 18:37:26 +0200

    Animal Test / Implementation TODO : TEST

--- src/main/java/fr/univavignon/rodeo/implementation/Animal.java
+++ src/main/java/fr/univavignon/rodeo/implementation/Animal.java
@@ -2,23 +2,19 @@ package fr.univavignon.rodeo.implementation;
 
 import fr.univavignon.rodeo.api.*;
 
-public class Animal implements IAnimal{
+public class Animal extends NamedObject implements IAnimal{
 
-	private String name;
 	private boolean boss,endangered,secret;
 	private int xp;
 	
 	public Animal(String _name, int _xp,boolean _boss,boolean _endangered,boolean _secret) {
-		name =_name;
+		super(_name);
 		xp=_xp;
 		boss=_boss;
 		endangered=_endangered;
 		secret=_secret;
 	}
 	
-	public String getName() {
-		return name;
-	}
 
 	public int getXP() {
 		return xp;
