95602d6ae6bc559ec22d5a5e768e83babd3d7539
AIT MANSOUR Mohamed <mohamedaitmansour95@gmail.com>
2018-04-14 19:06:07 +0200

    perfection ?

---
+++ src/test/java/fr/univavignon/rodeo/SpecieLevelTest.java
@@ -0,0 +1,16 @@
+package fr.univavignon.rodeo;
+import org.junit.Test;
+
+import fr.univavignon.rodeo.api.SpecieLevel;
+
+public class SpecieLevelTest {
+	//Perfection ?
+	@Test
+	public void testSpecieLevel(){
+		SpecieLevel.valueOf(SpecieLevel.NOVICE.toString());
+		SpecieLevel.valueOf(SpecieLevel.WRANGLER.toString());
+		SpecieLevel.valueOf(SpecieLevel.CHAMPION.toString());
+		SpecieLevel.valueOf(SpecieLevel.MASTER.toString());		
+	}
+
+}
