9462d374e0a8b7a35bdd0675ea49b8c5e0dec9dc
AIT MANSOUR Mohamed <mohamedaitmansour95@gmail.com>
2018-04-14 19:30:54 +0200

    P_E_R_F_E_C

--- src/test/java/fr/univavignon/rodeo/SpecieLevelTest.java
+++ src/test/java/fr/univavignon/rodeo/SpecieLevelTest.java
@@ -9,7 +9,7 @@ public class SpecieLevelTest {
 	//Perfection ?
 	@Test
 	public void testSpecieLevel(){
-		assertEquals(0,SpecieLevel.NOVICE);
+		assertEquals("NOVICE",SpecieLevel.NOVICE.toString());
 		SpecieLevel.valueOf(SpecieLevel.WRANGLER.toString());
 		SpecieLevel.valueOf(SpecieLevel.CHAMPION.toString());
 		SpecieLevel.valueOf(SpecieLevel.MASTER.toString());		
